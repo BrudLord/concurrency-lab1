479db811e2b5519c370073fc95663d76d79cc453
brudlord <brudloord@yandex.ru>
2026-09-12 14:31:32 +0300

    Make

--- src/main/java/org/labs/Main.java
+++ src/main/java/org/labs/Main.java
@@ -1,7 +1,14 @@
 package org.labs;
 
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        Playground playground = new Playground(10, 10, 1000000);
+        List<Integer> res = playground.start();
+        for (Integer val : res) {
+            System.out.println(val);
+        }
     }
 }
---
+++ src/main/java/org/labs/Progger.java
@@ -0,0 +1,43 @@
+package org.labs;
+
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Progger implements Callable<Integer> {
+    private ExecutorService waitersThreadPool;
+    private Waiter waiter;
+    private Integer eaten = 0;
+    private ReentrantLock firstSpoon;
+    private ReentrantLock secondSpoon;
+
+    public Progger(ExecutorService waitersThreadPool, Waiter waiter, Integer number, List<ReentrantLock> spoons) {
+        this.waitersThreadPool = waitersThreadPool;
+        this.waiter = waiter;
+        if (number.equals(0)) {
+            this.firstSpoon = spoons.get(0);
+            this.secondSpoon = spoons.get(spoons.size() - 1);
+        } else {
+            this.firstSpoon = spoons.get(number - 1);
+            this.secondSpoon = spoons.get(number);
+        }
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        while (true) {
+            Future<Integer> task = waitersThreadPool.submit(waiter);
+            Integer food = task.get();
+            if (food.equals(0)) {
+                return eaten;
+            }
+            firstSpoon.lock();
+            secondSpoon.lock();
+            eaten += 1;
+            secondSpoon.unlock();
+            firstSpoon.unlock();
+        }
+    }
+}
